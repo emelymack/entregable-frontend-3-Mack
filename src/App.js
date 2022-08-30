5f1644a92b21a60d2f9eb98ba75c5e55c4e2ce71
emelymack <emelymack@gmail.com>
2022-08-29 22:54:40 -0300

    Agrego el proyecto casi finalizado

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+
 // El componente App es el padre de:
 // - Cabecera
 // - Listado
@@ -6,11 +10,12 @@
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
 function App() {
+  const [productos, setProductos] = useState(0);
 
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera cantProductos={productos} />
+      <Listado agregarACarrito={() => setProductos(productos + 1)}/>
     </div>
   );
 }
